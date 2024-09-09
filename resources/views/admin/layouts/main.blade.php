1e7a5551979f1ca3b5d17d2ca885d84ade6c5cc7
Juan Pablo Rodriguez <pablojuan18sc35@gmail.com>
2024-09-08 22:05:04 -0400

    modifing details

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -10,12 +10,7 @@
 </head>
 
 <body>
-
-    <div class="antialiased bg-gray-50 dark:bg-gray-900">
-        @include('admin.layouts.header')
-        @include('admin.layouts.sidebar')
-    </div>
-
+    @include('admin.layouts.header-sidebar')
     <main class="px-4 pt-20  md:ml-64 h-auto">
         <div class="shadow rounded-lg border-gray-300 dark:border-gray-600 h-28 mb-4">
             @yield('admin-content-toolbar')
@@ -23,14 +18,10 @@
         <div class="shadow rounded-lg border-gray-300 dark:border-gray-600 h-screen mb-4">
             @yield('admin-content')
         </div>
-
     </main>
-
     <div class="px-4 pb-4 md:ml-64 h-auto">
         @include('admin.layouts.footer')
     </div>
-
-
 
     @include('global.layouts.scripts')
 </body>
