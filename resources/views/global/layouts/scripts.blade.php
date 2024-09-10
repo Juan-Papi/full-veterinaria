a7425794173e0fb9c8a2861595c8cd48793904c1
Juan Pablo Rodriguez <pablojuan18sc35@gmail.com>
2024-09-09 20:55:30 -0400

    spinner added

--- resources/views/global/layouts/scripts.blade.php
+++ resources/views/global/layouts/scripts.blade.php
@@ -1,3 +1,23 @@
 <script src="{{ asset('plugins/global/jquery-3.7.1.min.js') }}"></script>
 <script src="{{ asset('template-admin/assets/plugins/global/flowbite-2.5.1.min.js') }}"></script>
+
+<script>
+    window.onbeforeunload = function() {
+        mostrarLoading();
+    };
+
+    document.addEventListener('DOMContentLoaded', () => {
+        ocultarLoading();
+    });
+
+    const mostrarLoading = () => {
+        console.log("Mostrando spinner...");
+        $('#loading').show();
+    };
+
+    const ocultarLoading = () => {
+        console.log("Ocultando spinner...");
+        $('#loading').hide();
+    };
+</script>
 @stack('scripts')
--- resources/views/global/layouts/styles.blade.php
+++ resources/views/global/layouts/styles.blade.php
@@ -4,5 +4,35 @@
 <!--begin::Fonts(mandatory for all pages)-->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
 <!--end::Fonts-->
-<style></style>
+<style>
+    .spinner svg {
+        width: 50px;
+        /* Tamaño más grande para mejor visibilidad */
+        height: 50px;
+        fill: #007bff;
+        /* Un color azul que debería contrastar bien con ambos fondos */
+    }
+
+    #loading {
+        background-color: rgba(0, 0, 0, 0.5);
+        /* Fondo semitransparente */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        inset: 0;
+        /* Esto cubrirá toda la pantalla */
+        z-index: 9999;
+    }
+
+    @keyframes spinner-b87k6z {
+        to {
+            transform: rotate(360deg);
+        }
+    }
+
+    .spinner {
+        animation: spinner-b87k6z 1s infinite linear;
+    }
+</style>
 @stack('styles')
